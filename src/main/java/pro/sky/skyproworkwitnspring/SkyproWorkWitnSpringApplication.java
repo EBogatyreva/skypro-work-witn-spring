1a53e6863f0d2ae4f67c1c910705150985b92b24
L <EBogatyrevaPK@yandex.ru>
2022-04-23 22:44:55 +1200

    )

---
+++ src/main/java/pro/sky/skyproworkwitnspring/SkyproWorkWitnSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproworkwitnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproWorkWitnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproWorkWitnSpringApplication.class, args);
+	}
+
+}
